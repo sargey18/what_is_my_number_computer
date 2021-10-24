f117df2dc194c40e6af9613f6d7e72a9dbefbcde
Ryan Sargeson <ryanteachviet@gmail.com>
2021-10-24 20:50:13 +0000

    removed the old function and called the new function

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 import random  
 
-def guess(x):   
-    random_number = random.randint(1, x) 
-                                              
-    guess = 0                                             
-    
-    while guess != random_number: 
-       guess = int(input(f'Guess a number between 1 and {x}: ')) 
-       if guess < random_number:
-           print('Sorry, guess again. Too low. ')
-       elif guess > random_number:                       
-           print('Sorry, guess again. Too high. ')
-    
-    print(f'Yay, congrats. You have guessed the number {random_number} get in lad')
+
 
 def computer_guess(x):
     low = 1   # we need to set the range that we will be dealing with 
@@ -33,6 +21,6 @@ def computer_guess(x):
     print(f'Yay! The computer guessed your number {guess}, correctly! ')  # guess was the last number 
     
 
+computer_guess(10)
 
-
-guess(10)  
+# guess(10)  
